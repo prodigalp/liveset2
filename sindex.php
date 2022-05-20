5b561555a03dab97fed32609d21b3c2d1684049e
eyestrain <prodigalp@gmail.com>
2022-05-21 00:14:00 +0800

    Added Three COlumns

--- sindex.php
+++ sindex.php
@@ -12,6 +12,20 @@
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quisquam soluta quasi voluptas suscipit minus quam at, ipsam ad dolorum rerum debitis perspiciatis dolore cum corrupti necessitatibus velit quis asperiores.
             Eum tenetur similique excepturi quis incidunt. Accusantium tempora consectetur excepturi sequi incidunt? Modi perspiciatis deserunt amet corporis nobis facilis ullam commodi veritatis dolores, iusto doloribus molestias est esse, eos reprehenderit.</p>
             <footer>Copyright 2022</footer>
+            <div class="triColumn">
+                <div class="colOne">
+                    <h2>Column One</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis tempora, quibusdam incidunt ut quisquam aut omnis sit sed assumenda ab soluta autem. Unde cupiditate quos nam perspiciatis quasi vero? Qui.</p>
+                </div>
+                <div class="colTwo">
+                    <h2>Column Two</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam eveniet voluptates hic fugit dolore ab corrupti! Tempore consequatur assumenda totam natus, deserunt officiis harum amet ipsa provident! Labore, aut voluptatem?</p>
+                </div>
+                <div class="colTri">
+                    <h2>Column Three</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error inventore dolorum, atque ad harum iste similique debitis delectus corporis praesentium, doloribus, autem incidunt accusantium quo vitae quaerat. Sequi, eligendi commodi!</p>
+                </div>
+            </div>
         </div>
     </body>
 </html>
