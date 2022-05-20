46e25b8ca25ae137cf2352e83f4161e5c72daa6d
eyestrain <prodigalp@gmail.com>
2022-05-20 23:18:52 +0800

    Added Primary Header and Paragraph

--- sindex.php
+++ sindex.php
@@ -6,4 +6,11 @@
         <title>Initial Boiler plate</title>
         <link rel="stylesheet" href="style.css">
     </head>
+    <body>
+        <div id="wrapper">
+            <h2>Primary Header</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quisquam soluta quasi voluptas suscipit minus quam at, ipsam ad dolorum rerum debitis perspiciatis dolore cum corrupti necessitatibus velit quis asperiores.
+            Eum tenetur similique excepturi quis incidunt. Accusantium tempora consectetur excepturi sequi incidunt? Modi perspiciatis deserunt amet corporis nobis facilis ullam commodi veritatis dolores, iusto doloribus molestias est esse, eos reprehenderit.</p>
+        </div>
+    </body>
 </html>
